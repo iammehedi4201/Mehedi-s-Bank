baadd113179a640f13f6cc0961813c2af95eb375
Mehedi <iammehedi296@gmail.com>
2021-05-08 23:02:37 +0600

    I finished some of it work

---
+++ script.js
@@ -0,0 +1,8 @@
+let enterButton = document.getElementById("EnterButton");
+
+enterButton.addEventListener("click",function()
+{
+    let loginContainer = document.getElementById("login-container");
+    
+    loginContainer.style.display="none";
+})
