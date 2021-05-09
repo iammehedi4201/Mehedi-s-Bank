de66cdec25e77121f2e71e2ad014c29e452a42bb
Mehedi <iammehedi296@gmail.com>
2021-05-09 13:09:19 +0600

    I create Mehedi's Bank

--- script.js
+++ script.js
@@ -1,8 +1,67 @@
 let enterButton = document.getElementById("EnterButton");
 
+//Enter Button Event Handler
+
 enterButton.addEventListener("click",function()
 {
     let loginContainer = document.getElementById("login-container");
     
     loginContainer.style.display="none";
+
+    let tansactionContainer = document.getElementById("transaction-container");
+
+    tansactionContainer.style.display="block";
 })
+
+//Deposit Button Event Handler
+
+let depositButton = document.getElementById("deposit-button");
+
+depositButton.addEventListener("click",function () {
+
+   let depositAmountNumber = getInputValue(("deposit-input"));
+
+   updateDepositAndWithdrawAndBalnce ("current-deposit",depositAmountNumber)
+
+   document.getElementById("deposit-input").value = ''; 
+})
+
+//withdraw Event Handler 
+let withdrawButton = document.getElementById("withdraw-button");
+
+withdrawButton.addEventListener("click",function () 
+{
+   let withdrawAmountNumber = getInputValue("withdraw-input");
+
+   updateDepositAndWithdrawAndBalnce("current-withdraw",withdrawAmountNumber)
+
+   updateDepositAndWithdrawAndBalnce("current-blance",-1 * withdrawAmountNumber)
+
+   document.getElementById("withdraw-input").value = ''; 
+})
+
+//getInputValue function
+
+function getInputValue(id)
+{
+    let depositAmount = document.getElementById(id).value;
+
+    let depositAmountNumber = parseFloat(depositAmount);
+    
+    return depositAmountNumber;
+}
+
+//updateDepositAndWithdrawAndBalnce 
+
+function updateDepositAndWithdrawAndBalnce (id,depositAmountNumber) 
+{
+    
+   let currentAmount = document.getElementById(id).innerText;
+    
+   let  currentAmountNumber = parseFloat(currentAmount);
+
+   let totalAmount = depositAmountNumber + currentAmountNumber;
+
+   document.getElementById(id).innerText = totalAmount;
+
+}
